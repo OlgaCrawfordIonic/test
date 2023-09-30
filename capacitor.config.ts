364022da3b320fdcc942b9ebfd1cee753b0c8cb2
Olga-Crawford <olgacrawford100@yahoo.com>
2023-09-30 15:28:00 +0100

    first commit

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'io..starter',
   appName: 'test',
   webDir: 'www',
   server: {
